b06e45261154fd04a603021245908d47ab81837a
Valentin Kuznetsov <vkuznet@gmail.com>
2019-02-24 12:12:23 -0500

    New exporter to monitoring specific PID

---
+++ process_exporter.go
@@ -0,0 +1,42 @@
+package main
+
+// Author: Valentin Kuznetsov <vkuznet [AT] gmail {DOT} com>
+// Example of cmsweb data-service exporter for prometheus.io
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+)
+
+var (
+	listeningAddress = flag.String("address", ":7100", "Address on which to expose metrics and web interface.")
+	metricsEndpoint  = flag.String("endpoint", "/metrics", "Path under which to expose metrics.")
+	scrapeURI        = flag.String("uri", "", "URI of server status page we're going to scrape")
+	pid              = flag.Int("pid", 0, "PID of the process to scrape")
+	prefix           = flag.String("prefix", "process_exporter", "Assign prefix to prometheus reports to monitor this PID")
+)
+
+// main function
+func main() {
+	flag.Parse()
+	if *pid == 0 {
+		fmt.Println("Please provide valid PID")
+		os.Exit(1)
+	}
+	opts := prometheus.ProcessCollectorOpts{
+		PidFn:        func() (int, error) { return *pid, nil },
+		Namespace:    *prefix,
+		ReportErrors: true,
+	}
+	exporter := prometheus.NewProcessCollector(opts)
+	prometheus.MustRegister(exporter)
+
+	log.Infof("Starting Server: %s", *listeningAddress)
+	http.Handle(*metricsEndpoint, prometheus.Handler())
+	log.Fatal(http.ListenAndServe(*listeningAddress, nil))
+}
