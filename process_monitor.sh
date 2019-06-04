7172f9c4e07151540abd280e25fa9073e25b795b
Valentin Kuznetsov <vkuznet@gmail.com>
2019-06-04 10:46:28 -0400

    Identify UNIX processes (cmsweb) based on provided pid file

--- process_monitor.sh
+++ process_monitor.sh
@@ -7,8 +7,14 @@
 usage="Usage: process_monitor.sh <pattern> <prefix> <address> <interval>"
 if [ $# -ne 4 ]; then
     echo $usage
-    echo "Example: start monitor for scitoken UNIX process on address :18883"
+    echo ""
+    echo "For example, start UNIX process monitoring on address :18883 with 15 sec interval"
+    echo ""
+    echo "# monitor process with given scitoken pattern"
     echo "process_monitor.sh \".*scitoken\" scitoken \":18883\" 15"
+    echo ""
+    echo "# monitor crabserver via provide pid file"
+    echo "process_monitor.sh /data/srv/state/crabserver/pid scitoken \":18883\" 15"
     exit 1
 fi
 if [ "$1" == "-h" ] || [ "$1" == "-help" ] || [ "$1" == "--help" ]; then
@@ -23,8 +29,20 @@ address=$3
 interval=$4
 while :
 do
-    # find pid of our pattern
-    pid=`ps auxw | grep "$pat" | grep -v grep | grep -v process_exporter | grep -v process_monitor | grep -v rotatelogs | awk '{print $2}'`
+    # start with empty pid
+    pid=""
+    # if pat is an existing file, e.g. /data/srv/state/reqmgr2/pid
+    # we'll use it to find out group pid
+    if [ -f $pat ]; then
+        gid=`cat $pat`
+        pid=`ps -g $gid | grep -v PID | awk '{print $1}' | sort | tail -1`
+        if [ -z "$pid" ]; then
+            pid=`ps -p $gid | grep -v PID | awk '{print $1}' | sort | tail -1`
+        fi
+    else
+        # find pid of our pattern
+        pid=`ps auxw | grep "$pat" | grep -v grep | grep -v process_exporter | grep -v process_monitor | grep -v rotatelogs | awk '{print $2}'`
+    fi
     if [ -z "$pid" ]; then
         echo "No pattern '$pat' found"
         sleep $interval
